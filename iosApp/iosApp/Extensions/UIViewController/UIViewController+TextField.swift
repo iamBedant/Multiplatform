37bec696996901358056701a83f50612073007b3
mohit anand <mohitanand95.ma@gmail.com>
2018-12-03 03:50:35 +0530

    basic poc done

---
+++ iosApp/iosApp/Extensions/UIViewController/UIViewController+TextField.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewController+TextField.swift
+//  iosApp
+//
+//  Created by Mohit Anand on 03/12/18.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
